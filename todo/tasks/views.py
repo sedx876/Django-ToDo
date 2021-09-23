cc2042159c39fdf41a8e4bb9b51dd3a5abc5408f
sedx876 <swatkins876@gmail.com>
2021-09-23 09:46:19 -0400

    Update

---
+++ todo/tasks/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+
+
+def index(request):
+    tasks = Task.objects.all()
+    context = {'tasks': tasks}
+    return render(request, 'tasks/list.html', context)
